be240953c90839fbb8fc13f4748e76dc6c5bc887
r3drighthand <ryanmccool13@gmail.com>
2017-09-01 15:10:09 -0500

    add migration, model for Card

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,7 +1,7 @@
-get '/decks' do
-  @decks = Deck.all
-  erb :'decks/index'
-end
+# get '/decks' do
+#   @decks = Deck.all
+#   erb :'decks/index'
+# end
 
 # DECKS show
 # get '/decks/:id' do
---
+++ db/migrate/20170901150609_create_card.rb
@@ -0,0 +1,11 @@
+class CreateCard < ActiveRecord::Migration[5.1]
+  def change
+    create_table :cards do |t|
+      t.string :question
+      t.string :answer
+      t.integer :deck_id
+
+      t.timestamps
+    end
+  end
+end
