b3e1ca61a387742d7e5553452a39831a74672256
Marla Kallstrom <brandykou@gmail.com>
2017-09-01 15:18:02 -0500

    add password hashing

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ActiveRecord::Base
 
   validates :username, :email, presence: true
   validates :username, :email, uniqueness: true
+
+  def password
+    @password ||= BCrypt::Password.new(hashed_pw)
+  end
+
+  def password=(new_password)
+    self.hashed_pw = BCrypt::Password.create(new_password)
+  end
+
 end
