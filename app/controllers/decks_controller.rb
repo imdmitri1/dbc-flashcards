565684c158978b93c822f8ddcf90a7b7396af716
Marla Kallstrom <brandykou@gmail.com>
2017-09-02 17:25:08 -0500

    get sliced routes working together

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -5,6 +5,12 @@ end
 
 # DECKS show
 get '/decks/:id' do
+  #show the stats or something for the deck
   @deck = Deck.find(params[:id])
   erb :'decks/show'
+end
+
+get '/decks/:id/rounds' do
+  @round = Round.new
+  redirect "/rounds/#{@round.id}/decks/params[:id]"
 end
--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -3,16 +3,16 @@ get '/rounds/:round_id/decks/:deck_id' do
 
   @round = Round.find(params[:round_id])
   @card = @round.remaining_cards.sample
-  erb :"show.erb"
+  erb :show
 end
 
 post '/rounds/:round_id/cards/:card_id/guesses' do
   # make a guess for this card, in this round
-  correct_answer = Card.find_by(id: params[:card_id]).answer
+  correct_answer = Card.find(params[:card_id]).answer
   if params[:answer] == correct_answer
-    Guess.create(card_id: params[:card_id], guess: params[:answer], true_false: "true", round_id: params[:round_id])
+    @guess = Guess.create(card_id: params[:card_id], guess: params[:answer], true_false: "true", round_id: params[:round_id])
   else
-     Guess.create(card_id: params[:card_id], guess: params[:answer], true_false: "false", round_id: params[:round_id])
+     @guess = Guess.create(card_id: params[:card_id], guess: params[:answer], true_false: "false", round_id: params[:round_id])
   end
-  redirect '/rounds/:round_id/decks/:deck_id'
+  redirect "/rounds/#{@guess.round_id}/decks/#{@correct_answer.deck_id}"
 end
