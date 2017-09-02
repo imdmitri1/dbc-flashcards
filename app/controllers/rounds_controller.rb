b91070492b9a969dca16b4d946cd35fc226b172a
Marla Kallstrom <brandykou@gmail.com>
2017-09-02 18:55:22 -0500

    can update attempt count and true/false

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -8,12 +8,24 @@ end
 
 post '/rounds/:round_id/decks/:deck_id/cards/:card_id' do
   # make a guess for this card, in this round
-  correct_answer = Card.find(params[:card_id]).answer
+  @card = Card.find(params[:card_id])
+  correct_answer = @card.answer
   #this for if there is no guess for the card yet:
-  if params[:answer] == correct_answer
-    @guess = Guess.create(card_id: params[:card_id], guess: params[:answer], true_false: "true", round_id: params[:round_id], attempt: 1)
+  @guess = Guess.find_by(card_id: @card.id, round_id: params[:round_id])
+  if @guess
+    if params[:answer] == correct_answer
+      @guess.true_false = "true"
+      @guess.attempt += 1
+    else
+      @guess.attempt +=1
+    end
+    @guess.save
   else
-     @guess = Guess.create(card_id: params[:card_id], guess: params[:answer], true_false: "false", round_id: params[:round_id], attempt: 1)
+    if params[:answer] == correct_answer
+      @guess = Guess.create(card_id: params[:card_id], true_false: "true", round_id: params[:round_id], attempt: 1)
+    else
+       @guess = Guess.create(card_id: params[:card_id], true_false: "false", round_id: params[:round_id], attempt: 1)
+    end
   end
   redirect "/rounds/#{params[:round_id]}/decks/#{params[:deck_id]}"
 end
