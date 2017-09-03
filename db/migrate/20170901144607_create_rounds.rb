d83e2d88a3d3bc0bfad25d4158b3a789f53393f5
Marla Kallstrom <brandykou@gmail.com>
2017-09-03 13:22:17 -0500

    remove not null constraint from rounds table

--- db/migrate/20170901144607_create_rounds.rb
+++ db/migrate/20170901144607_create_rounds.rb
@@ -2,7 +2,7 @@ class CreateRounds < ActiveRecord::Migration[5.1]
   def change
     create_table :rounds do |t|
       t.integer :deck_id, null: false
-      t.integer :user_id, null: false
+      t.integer :user_id
 
       t.timestamps
     end
